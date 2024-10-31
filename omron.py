f32d83e8c9779b550b1e5071400c19c18ff70a1f
Adrian Wiedemann <github-720656@kyubu.de>
2024-10-31 21:40:42 +0100

    Strip comments

--- omron.py
+++ omron.py
@@ -11,22 +11,30 @@ from rawreader import basereader, sigrok
 
 logger = logging.getLogger()
 
+
 def main():
 
     ap = argparse.ArgumentParser()
-    # ap.add_argument("--raw-input", action="store_true", default=False, help="Set if data is raw input (bitstring)")
-    # ap.add_argument("--raw-type", choices=["csv", "oneline", "serial"], default=None)
-    # ap.add_argument("--type", choices=["msr100"], default=None)
-    ap.add_argument("--input", choices=["msr100", "ardumsr", "sigrok_csv", "oneline"], default="msr100")
-    ap.add_argument("--input-file", type=argparse.FileType("rt"), help="A file containg the data for reading. Use '-' for stdin.")
-    ap.add_argument("--input-track", choices=[1, 2, 3], default=2, help="The input track to use to decode the raw input")
-    ap.add_argument("--track1-processor", choices=["noop", "iso7813", "bahn"], default="noop", help="processor for track 1")
-    ap.add_argument("--track2-processor", choices=["noop", "iso7813", "bahn"], default="noop", help="processor for track 2")
-    ap.add_argument("--track3-processor", choices=["noop", "iso4909", "girocard", "bahn"], default="noop", help="processor for track 3")
-    ap.add_argument("--remap-to-us", action="store_true", default=False, help="Remap scancodes to US keyboard layout")
-    ap.add_argument("--loop", action="store_true", default=False, help="Loop reading cards")
-    ap.add_argument("--print-verbose", action="store_true", default=False, help="Print verbose trackd data")
-    ap.add_argument("--log-level", choices=list(logging.getLevelNamesMapping().keys()), default=logging.INFO, help="Print verbose trackd data")
+    ap.add_argument(
+        "--input", choices=["msr100", "ardumsr", "sigrok_csv", "oneline"], default="msr100")
+    ap.add_argument("--input-file", type=argparse.FileType("rt"),
+                    help="A file containg the data for reading. Use '-' for stdin.")
+    ap.add_argument("--input-track", choices=[1, 2, 3], default=2,
+                    help="The input track to use to decode the raw input")
+    ap.add_argument("--track1-processor", choices=[
+                    "noop", "iso7813", "bahn"], default="noop", help="processor for track 1")
+    ap.add_argument("--track2-processor", choices=[
+                    "noop", "iso7813", "bahn"], default="noop", help="processor for track 2")
+    ap.add_argument("--track3-processor", choices=[
+                    "noop", "iso4909", "girocard", "bahn"], default="noop", help="processor for track 3")
+    ap.add_argument("--remap-to-us", action="store_true",
+                    default=False, help="Remap scancodes to US keyboard layout")
+    ap.add_argument("--loop", action="store_true",
+                    default=False, help="Loop reading cards")
+    ap.add_argument("--print-verbose", action="store_true",
+                    default=False, help="Print verbose trackd data")
+    ap.add_argument("--log-level", choices=list(logging.getLevelNamesMapping().keys()),
+                    default=logging.INFO, help="Print verbose trackd data")
     args = ap.parse_args()
 
     logging.basicConfig(level=logging.getLevelName(args.log_level))
@@ -34,7 +42,7 @@ def main():
     while True:
 
         bitstring = None
-        trackdata = (None, None, None) # The decoded data from the tracks
+        trackdata = (None, None, None)  # The decoded data from the tracks
         error_reading = False
 
         try:
@@ -45,7 +53,9 @@ def main():
                 rdr = sigrok.CsvReader(args.input_file)
                 bitstring = rdr.read_input()
             elif args.input == "ardumsr":
-                rdr = basereader.SerialReader(port="/dev/ttyACM0", baudrate=9600) # The port the Arduino provides
+                # The port the Arduino provides
+                rdr = basereader.SerialReader(
+                    port="/dev/ttyACM0", baudrate=9600)
                 bitstring = rdr.read_input()
             elif args.input == "msr100":
                 # No raw reader, decode directly
@@ -55,31 +65,14 @@ def main():
                 print(f"Unsupported input: {args.type}")
                 error_reading = True
         except Exception as e:
-                print(f"Error reading: {e}")
-                error_reading = True
+            print(f"Error reading: {e}")
+            error_reading = True
         finally:
             if error_reading:
                 sys.exit(1)
 
-        # if args.raw_type == "oneline":
-        #     rdr = datareader.FirstLine(args.data)
-        #     bitstring = rdr.read_input()
-        # elif args.raw_type == "csv":
-        #     rdr = sigrok.CsvReader(args.data)
-        #     bitstring = rdr.read_input()
-        # elif args.raw_type == "serial":
-        #     try:
-        #         rdr = basereader.SerialReader("/dev/ttyACM0") # The port the Arduino provides
-        #         bitstring = datareader.read_serial("/dev/ttyACM0")
-        #     except serial.serialutil.SerialException as e:
-        #         logging.error("Error reading serial port: %s", e)
-        #         sys.exit(1)
-        # elif args.type == "msr100":
-        #     rdr = msr100.Decoder(args.remap_to_us)
-        #     trackdata = rdr.read_input()
-
         if bitstring is not None:
-            dec = iso7813.Decoder(args.input_track) # Decode as track n
+            dec = iso7813.Decoder(args.input_track)  # Decode as track n
             trackdata = dec.decode_bitstring(bitstring)
             print(f"Decoded track {args.input_track} data: '{trackdata[1]}'")
             # TODO: decode LRC here
@@ -118,28 +111,17 @@ def main():
                 parser = girocard.Parser()
                 printer = girocard.Printer(print_verbose=args.print_verbose)
             else:
-                print(f"Unsupported processor for track {trackno}: {processor_choice}")
+                print(f"Unsupported processor for track {
+                      trackno}: {processor_choice}")
                 continue
 
             trackdata_details = parser.process_trackdata(trackdata_copy)
             if printer is not None:
                 printer.print_trackdata(trackdata_details)
 
-        # parser = iso7813.Parser()
-        # # parser = bahn.Parser()
-        # trackdata_details = parser.process_trackdata(trackdata)
-
-        # printer = iso7813.Printer(print_verbose=args.print_verbose)
-        # # printer = bahn.Printer(print_verbose=True)
-        # printer.print_trackdata(trackdata_details)
-
-        # parsertrack3 = girocard.Parser()
-        # track3 = parsertrack3.process_trackdata(trackdata)
-        # printer = girocard.Printer(print_verbose=args.print_verbose)
-        # printer.print_trackdata(track3)
-
         if not args.loop:
             break
+
 
 if __name__ == "__main__":
     main()
